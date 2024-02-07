c4fdafe330202ca0f5488adbfc4160340b96445a
Edward Apostol <ed@edapostol.com>
2024-02-07 15:06:26 -0500

    dev: added flex-grow

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-let btns = document.querySelector('.btns').querySelectorAll('input'),
+let input = document.querySelector('.btns').querySelector('input'),
     dirs = document.querySelector('.dir').querySelectorAll('input'),
-    container = document.querySelector('.container');
+    container = document.querySelector('.container'),
+    flexitem = document.querySelector('.extra');
 
-for (i of btns) {
-  (function(i) {
-    i.addEventListener('click', function() {
-      clearClasses(container, btns);
-      container.classList.add(i.value);
-    }, false);
-  })(i);
-}
+input.addEventListener('input', function() {
+  flexitem.style.flexGrow = this.value;
+}, false);
 
+let i;
 for (i of dirs) {
   (function(i) {
     i.addEventListener('click', function() {
