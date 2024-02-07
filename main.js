3d2aea89c7f208e1b5c30fb3184fbc5a8bbfbb3f
Edward Apostol <ed@edapostol.com>
2024-02-07 16:30:12 -0500

    dev: added sticky footer example

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-let input = document.querySelector('.btns').querySelector('input'),
-    dirs = document.querySelector('.dir').querySelectorAll('input'),
-    container = document.querySelector('.container'),
-    flexitem = document.querySelector('.extra'), i;
-
-input.addEventListener('input', function() {
-  flexitem.style.flexGrow = this.value;
+document.querySelector('button').addEventListener('click', function () {
+  document.querySelector('main')
+      .appendChild(document.querySelector('p').cloneNode(true));
 }, false);
 
-for (i of dirs) {
-  (function(i) {
-    i.addEventListener('click', function() {
-      clearClasses(container, dirs);
-      container.classList.add(i.value);
-    }, false);
-  })(i);
-}
-
-function clearClasses (el, cl) {
-  for (i of cl) {
-    el.classList.remove(i.value);
+document.querySelectorAll('button')[1].addEventListener('click', function () {
+  if (document.querySelectorAll('p')[1]) {
+    document.querySelector('p').remove();
   }
-}
+
+}, false);
